2b6f7780c8586cd76755416ba6c5b5c8051fe9bf
cecsr <cecsr@DESKTOP-3Q2DI72>
2024-02-12 11:25:07 -0700

    Add project files.

---
+++ JN-Mission6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JN_Mission6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
