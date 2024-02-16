58b0d5539acdb7d0e1f2f9b63cfa411ac10431c0
cecsr <cecsr@DESKTOP-3Q2DI72>
2024-02-16 10:51:28 -0700

    null values working

--- JN-Mission6/Migrations/20240216172959_Initial.cs
+++ JN-Mission6/Migrations/20240216172959_Initial.cs
@@ -21,9 +21,9 @@ namespace JN_Mission6.Migrations
                     Year = table.Column<string>(type: "TEXT", nullable: false),
                     Director = table.Column<string>(type: "TEXT", nullable: false),
                     Rating = table.Column<string>(type: "TEXT", nullable: false),
-                    Edited = table.Column<bool>(type: "INTEGER", nullable: false),
-                    LentTo = table.Column<bool>(type: "INTEGER", nullable: false),
-                    Notes = table.Column<string>(type: "TEXT", nullable: false)
+                    Edited = table.Column<bool>(type: "INTEGER", nullable: true),
+                    LentTo = table.Column<bool>(type: "INTEGER", nullable: true),
+                    Notes = table.Column<string>(type: "TEXT", nullable: true)
                 },
                 constraints: table =>
                 {
--- JN-Mission6/Models/MovieSubmit.cs
+++ JN-Mission6/Models/MovieSubmit.cs
@@ -11,8 +11,8 @@ namespace JN_Mission6.Models
         public string Year { get; set; }
         public string Director { get; set; }
         public string Rating { get; set; }
-        public bool Edited { get; set; }
-        public bool LentTo { get; set; }
-        public string Notes { get; set; }
+        public bool? Edited { get; set; }
+        public bool? LentTo { get; set; }
+        public string? Notes { get; set; }
     }
 }
